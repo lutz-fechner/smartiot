b83792fe3408ea593e1636d94a5a53a2895055c3
Lutz Fechner <lutz.fechner@googlemail.com>
2020-07-29 23:39:14 +0200

    Added FanControl Blazor App

---
+++ dotnetcore/FanControl/FanControl/Data/CounterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanControl.Data
+{
+    public class CounterService
+    {
+        public int Count { get; private set; }
+        public void Increment()
+        {
+            Count += 1;
+        }
+    }
+}
